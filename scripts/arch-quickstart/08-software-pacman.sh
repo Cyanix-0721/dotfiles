53d752c9935bb987f9d5ad96f024d12fb8977300
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2025-09-29 23:37:42 +0800

    chore: quickstart scripts

--- scripts/arch-quickstart/08-software-pacman.sh
+++ scripts/arch-quickstart/08-software-pacman.sh
@@ -6,5 +6,5 @@ echo "=== 安装常用软件 (Pacman) ==="
 
 # 安装常用软件 (Pacman)
 echo "安装常用软件 (Pacman)..."
-sudo pacman -S --noconfirm obsidian keepassxc vlc mpv 7zip yazi ffmpeg jq poppler resvg imagemagick neovim
+sudo pacman -S --noconfirm obsidian keepassxc vlc mpv 7zip yazi ffmpeg jq poppler resvg imagemagick neovim dex btop
 echo "✓ 常用软件 (Pacman) 安装完成"
