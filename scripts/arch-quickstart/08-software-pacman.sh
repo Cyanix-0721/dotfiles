86c50486f7e1a734cea9c53c3d2f286235899803
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2025-09-30 16:23:57 +0800

    feat: add github-cli

--- scripts/arch-quickstart/08-software-pacman.sh
+++ scripts/arch-quickstart/08-software-pacman.sh
@@ -6,5 +6,5 @@ echo "=== 安装常用软件 (Pacman) ==="
 
 # 安装常用软件 (Pacman)
 echo "安装常用软件 (Pacman)..."
-sudo pacman -S --noconfirm obsidian keepassxc vlc mpv 7zip yazi ffmpeg jq poppler resvg imagemagick neovim dex btop fastfetch
+sudo pacman -S --noconfirm obsidian keepassxc vlc mpv 7zip yazi ffmpeg jq poppler resvg imagemagick neovim dex btop fastfetch github-cli
 echo "✓ 常用软件 (Pacman) 安装完成"
