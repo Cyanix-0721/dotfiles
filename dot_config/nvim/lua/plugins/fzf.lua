462abbada87b67e3d61c519402479f6142aa5425
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2025-10-07 16:21:37 +0800

    feat: lazyvim plugins

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,13 +1,13 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  'ibhagwan/fzf-lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local fzf = require("fzf-lua")
+    local fzf = require('fzf-lua')
     fzf.setup({
       -- 在此处添加你的自定义配置
       -- 例如，设置文件搜索不忽略 .gitignore 中的文件
       files = {
-        fd = "--no-ignore-vcs",
+        fd = '--no-ignore-vcs',
       },
       -- 更多配置项请参考 :help fzf-lua
     })
