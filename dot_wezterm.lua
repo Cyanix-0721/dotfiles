75ac3911de1c8a0b3792b37f629e060f2780257b
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2026-04-03 18:06:57 +0800

    feat(config): add color scheme and font settings to wezterm config
    - set color_scheme to 'Tokyo Night'
    - set default font to JetBrainsMono Nerd Font Mono
    - configure font_locator: Windows uses Gdi, others FontConfig
    - add font_rules to render italic with Sarasa Mono SC

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,4 +4,15 @@ local config = wezterm.config_builder()
 local is_windows = wezterm.target_triple:find('windows') ~= nil
 config.default_prog = { is_windows and 'nu.exe' or 'nu' }
 
+config.color_scheme = 'Tokyo Night'
+
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+config.font_locator = is_windows and 'Gdi' or 'FontConfig'
+config.font_rules = {
+  {
+    font = wezterm.font('Sarasa Mono SC'),
+    italic = true,
+  },
+}
+
 return config
