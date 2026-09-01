56a7185e8afe7c1e2a497d09c42011dffcdbc7b5
Cyanix-0721 <34270450+Cyanix-0721@users.noreply.github.com>
2026-09-01 20:08:20 +0800

    feat(mise): 添加 mise 激活和补全功能的注释

--- dot_config/fish/conf.d/11-mise.fish
+++ dot_config/fish/conf.d/11-mise.fish
@@ -1,1 +1,9 @@
+# mise：激活 + shell 补全
+# mise: activation + shell completions
+
 mise activate fish | source
+
+# mise shell 补全 / mise shell completions
+if command -q mise
+    mise completion fish | source
+end
